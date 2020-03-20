31aab561014d159e8e66a1fe6a7222f937d774f1
Christopher Wilcox <crwilcox@google.com>
2020-03-20 11:18:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,11 +23,7 @@ import setuptools
 name = "google-cloud-firestore"
 description = "Google Cloud Firestore API client library"
 version = "1.6.2"
-# Should be one of:
-# 'Development Status :: 3 - Alpha'
-# 'Development Status :: 4 - Beta'
-# 'Development Status :: 5 - Production/Stable'
-release_status = "Development Status :: 4 - Beta"
+release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
     "google-api-core[grpc] >= 1.14.0, < 2.0.0dev",
     "google-cloud-core >= 1.0.3, < 2.0dev",
