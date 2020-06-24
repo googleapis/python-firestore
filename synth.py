5869d44c9fe473df2fb0ca9d8ab8c455bc92479e
Chris Wilcox <crwilcox@google.com>
2020-06-24 16:34:10 -0700

    rename test files

--- synth.py
+++ synth.py
@@ -40,7 +40,19 @@ for version in versions:
         f"google/cloud/firestore_{version}",
         excludes=[ library / f"google/firestore_{version}/__init__.py"]
     )
-    s.move(library / f"tests/unit/gapic")
+    
+    # Python Testing doesn't like modules named the same, can cause collisions in 
+    # import file mismatch:
+    # imported module 'test_firestore' has this __file__ attribute:
+    #   /Users/crwilcox/workspace/googleapis/python-firestore/tests/unit/gapic/firestore_v1/test_firestore.py
+    # which is not the same as the test file we want to collect:
+    # /Users/crwilcox/workspace/googleapis/python-firestore/tests/unit/gapic/firestore_v1beta1/test_firestore.py
+    # HINT: remove __pycache__ / .pyc files and/or use a unique basename for your test file modules
+    s.move(
+        library / f"tests/unit/gapic/firestore_{version}/test_firestore.py",
+        f"tests/unit/gapic/firestore_{version}/test_firestore_{version}.py"
+    )
+    
     s.move(library / "scripts/fixup_keywords.py", f"scripts/fixup_keywords_{version}.py" )
 
     s.replace(
