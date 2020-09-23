a41795551f7c7af9e779ad3fa0486c2b962d75ec
yoshi-automation <yoshi-automation@google.com>
2020-09-23 05:59:36 -0700

    Update gapic-generator-python pin to 0.33.4
    PiperOrigin-RevId: 333159182
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Sep 22 14:22:59 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 3dbeac0d54125b123c8dfd39c774b37473c36944
    Source-Link: https://github.com/googleapis/googleapis/commit/3dbeac0d54125b123c8dfd39c774b37473c36944

--- google/cloud/firestore_admin_v1/services/firestore_admin/client.py
+++ google/cloud/firestore_admin_v1/services/firestore_admin/client.py
@@ -19,10 +19,10 @@ from collections import OrderedDict
 from distutils import util
 import os
 import re
-from typing import Callable, Dict, Sequence, Tuple, Type, Union
+from typing import Callable, Dict, Optional, Sequence, Tuple, Type, Union
 import pkg_resources
 
-import google.api_core.client_options as ClientOptions  # type: ignore
+from google.api_core import client_options as client_options_lib  # type: ignore
 from google.api_core import exceptions  # type: ignore
 from google.api_core import gapic_v1  # type: ignore
 from google.api_core import retry as retries  # type: ignore
@@ -33,8 +33,8 @@ from google.auth.exceptions import MutualTLSChannelError  # type: ignore
 from google.oauth2 import service_account  # type: ignore
 
 from google.api_core import operation as ga_operation
-from google.api_core import operation
-from google.api_core import operation_async
+from google.api_core import operation  # type: ignore
+from google.api_core import operation_async  # type: ignore
 from google.cloud.firestore_admin_v1.services.firestore_admin import pagers
 from google.cloud.firestore_admin_v1.types import field
 from google.cloud.firestore_admin_v1.types import field as gfa_field
@@ -176,9 +176,9 @@ class FirestoreAdminClient(metaclass=FirestoreAdminClientMeta):
     def __init__(
         self,
         *,
-        credentials: credentials.Credentials = None,
-        transport: Union[str, FirestoreAdminTransport] = None,
-        client_options: ClientOptions = None,
+        credentials: Optional[credentials.Credentials] = None,
+        transport: Union[str, FirestoreAdminTransport, None] = None,
+        client_options: Optional[client_options_lib.ClientOptions] = None,
         client_info: gapic_v1.client_info.ClientInfo = DEFAULT_CLIENT_INFO,
     ) -> None:
         """Instantiate the firestore admin client.
@@ -192,8 +192,8 @@ class FirestoreAdminClient(metaclass=FirestoreAdminClientMeta):
             transport (Union[str, ~.FirestoreAdminTransport]): The
                 transport to use. If set to None, a transport is chosen
                 automatically.
-            client_options (ClientOptions): Custom options for the client. It
-                won't take effect if a ``transport`` instance is provided.
+            client_options (client_options_lib.ClientOptions): Custom options for the
+                client. It won't take effect if a ``transport`` instance is provided.
                 (1) The ``api_endpoint`` property can be used to override the
                 default endpoint provided by the client. GOOGLE_API_USE_MTLS_ENDPOINT
                 environment variable can also be used to override the endpoint:
@@ -219,9 +219,9 @@ class FirestoreAdminClient(metaclass=FirestoreAdminClientMeta):
                 creation failed for any reason.
         """
         if isinstance(client_options, dict):
-            client_options = ClientOptions.from_dict(client_options)
+            client_options = client_options_lib.from_dict(client_options)
         if client_options is None:
-            client_options = ClientOptions.ClientOptions()
+            client_options = client_options_lib.ClientOptions()
 
         # Create SSL credentials for mutual TLS if needed.
         use_client_cert = bool(
