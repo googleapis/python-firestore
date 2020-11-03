ef5f24a50e0a3268cf9b43b20022675aa1c722f5
Chris Wilcox <crwilcox@google.com>
2020-11-03 13:45:02 -0800

    chore: add trove classifier to setup.py, add pre-release test run for action

--- noxfile.py
+++ noxfile.py
@@ -27,9 +27,10 @@ PYTYPE_VERSION = "pytype==2020.7.24"
 BLACK_VERSION = "black==19.10b0"
 BLACK_PATHS = ["docs", "google", "tests", "noxfile.py", "setup.py"]
 
-DEFAULT_PYTHON_VERSION = "3.8"
-SYSTEM_TEST_PYTHON_VERSIONS = ["3.7"]
-UNIT_TEST_PYTHON_VERSIONS = ["3.6", "3.7", "3.8"]
+DEFAULT_PYTHON_VERSION = "3.9"
+SYSTEM_TEST_PYTHON_VERSIONS = ["3.9"]
+UNIT_TEST_PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9"]
+UNIT_TEST_PRERELEASE_PYTHON_VERSIONS = ["3.10"]
 
 
 @nox.session(python=DEFAULT_PYTHON_VERSION)
@@ -99,6 +100,10 @@ def default(session):
         *session.posargs,
     )
 
+@nox.session(python=UNIT_TEST_PRERELEASE_PYTHON_VERSIONS)
+def prerelease_unit(session):
+    """Run the unit test suite."""
+    default(session)
 
 @nox.session(python=UNIT_TEST_PYTHON_VERSIONS)
 def unit(session):
