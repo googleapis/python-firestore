ed9c292005c70299b2ddde23082ffd86b0684f26
Christopher Wilcox <crwilcox@google.com>
2020-11-03 15:04:20 -0800

    chore: remove stale comment

--- noxfile.py
+++ noxfile.py
@@ -29,8 +29,6 @@ BLACK_PATHS = ["docs", "google", "tests", "noxfile.py", "setup.py"]
 
 DEFAULT_PYTHON_VERSION = "3.9"
 SYSTEM_TEST_PYTHON_VERSIONS = ["3.9"]
-# Run unit tests on all supported versions. Also include a scenario to
-# test the next version.
 UNIT_TEST_PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9"]
 
 @nox.session(python=DEFAULT_PYTHON_VERSION)
