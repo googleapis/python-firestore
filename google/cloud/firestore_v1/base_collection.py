9ed5e87934b7b55b1dd91de31133c9389ab39ec5
Chris Wilcox <crwilcox@google.com>
2020-12-16 10:49:45 -0800

    chore: linty lint

--- google/cloud/firestore_v1/base_collection.py
+++ google/cloud/firestore_v1/base_collection.py
@@ -186,7 +186,7 @@ class BaseCollectionReference(object):
             # list_documents returns an iterator of document references, which do not
             # include any fields. To save on data transfer, we can set a field_path mask
             # to include no fields
-            "mask": {"field_paths": None}
+            "mask": {"field_paths": None},
         }
         kwargs = _helpers.make_retry_timeout_kwargs(retry, timeout)
 
