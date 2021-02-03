4673366c1efcb73b596470df99a4939cad035358
Craig Labenz <craiglabenz@google.com>
2021-02-03 12:44:20 -0800

    chore: manual synth

--- tests/unit/gapic/firestore_admin_v1/test_firestore_admin.py
+++ tests/unit/gapic/firestore_admin_v1/test_firestore_admin.py
@@ -98,6 +98,19 @@ def test__get_default_mtls_endpoint():
     )
 
 
+def test_firestore_admin_client_from_service_account_info():
+    creds = credentials.AnonymousCredentials()
+    with mock.patch.object(
+        service_account.Credentials, "from_service_account_info"
+    ) as factory:
+        factory.return_value = creds
+        info = {"valid": True}
+        client = FirestoreAdminClient.from_service_account_info(info)
+        assert client.transport._credentials == creds
+
+        assert client.transport._host == "firestore.googleapis.com:443"
+
+
 @pytest.mark.parametrize(
     "client_class", [FirestoreAdminClient, FirestoreAdminAsyncClient,]
 )
