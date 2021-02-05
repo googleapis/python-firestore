413c860ea7b618240efa840d5cf97a886cfce941
yoshi-automation <yoshi-automation@google.com>
2021-02-05 07:57:15 -0800

    chore: upgrade gapic-generator-python to 0.39.1
    feat: add 'from_service_account_info' factory to clients
    fix: fix sphinx identifiers
    PiperOrigin-RevId: 350246057
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Jan 5 16:44:11 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 520682435235d9c503983a360a2090025aa47cd1
    Source-Link: https://github.com/googleapis/googleapis/commit/520682435235d9c503983a360a2090025aa47cd1

--- tests/unit/gapic/firestore_admin_v1/test_firestore_admin.py
+++ tests/unit/gapic/firestore_admin_v1/test_firestore_admin.py
@@ -98,8 +98,21 @@ def test__get_default_mtls_endpoint():
     )
 
 
+def test_firestore_admin_client_from_service_account_info():
+    creds = credentials.AnonymousCredentials()
+    with mock.patch.object(
+        service_account.Credentials, "from_service_account_info"
+    ) as factory:
+        factory.return_value = creds
+        info = {"valid": True}
+        client = FirestoreAdminClient.from_service_account_info(info)
+        assert client.transport._credentials == creds
+
+        assert client.transport._host == "firestore.googleapis.com:443"
+
+
 @pytest.mark.parametrize(
-    "client_class", [FirestoreAdminClient, FirestoreAdminAsyncClient]
+    "client_class", [FirestoreAdminClient, FirestoreAdminAsyncClient,]
 )
 def test_firestore_admin_client_from_service_account_file(client_class):
     creds = credentials.AnonymousCredentials()
@@ -118,7 +131,10 @@ def test_firestore_admin_client_from_service_account_file(client_class):
 
 def test_firestore_admin_client_get_transport_class():
     transport = FirestoreAdminClient.get_transport_class()
-    assert transport == transports.FirestoreAdminGrpcTransport
+    available_transports = [
+        transports.FirestoreAdminGrpcTransport,
+    ]
+    assert transport in available_transports
 
     transport = FirestoreAdminClient.get_transport_class("grpc")
     assert transport == transports.FirestoreAdminGrpcTransport
@@ -2605,7 +2621,7 @@ def test_firestore_admin_host_with_port():
 
 
 def test_firestore_admin_grpc_transport_channel():
-    channel = grpc.insecure_channel("http://localhost/")
+    channel = grpc.secure_channel("http://localhost/", grpc.local_channel_credentials())
 
     # Check that channel is used if provided.
     transport = transports.FirestoreAdminGrpcTransport(
@@ -2617,7 +2633,7 @@ def test_firestore_admin_grpc_transport_channel():
 
 
 def test_firestore_admin_grpc_asyncio_transport_channel():
-    channel = aio.insecure_channel("http://localhost/")
+    channel = aio.secure_channel("http://localhost/", grpc.local_channel_credentials())
 
     # Check that channel is used if provided.
     transport = transports.FirestoreAdminGrpcAsyncIOTransport(
@@ -2642,7 +2658,7 @@ def test_firestore_admin_transport_channel_mtls_with_client_cert_source(
         "grpc.ssl_channel_credentials", autospec=True
     ) as grpc_ssl_channel_cred:
         with mock.patch.object(
-            transport_class, "create_channel", autospec=True
+            transport_class, "create_channel"
         ) as grpc_create_channel:
             mock_ssl_cred = mock.Mock()
             grpc_ssl_channel_cred.return_value = mock_ssl_cred
@@ -2698,7 +2714,7 @@ def test_firestore_admin_transport_channel_mtls_with_adc(transport_class):
         ssl_credentials=mock.PropertyMock(return_value=mock_ssl_cred),
     ):
         with mock.patch.object(
-            transport_class, "create_channel", autospec=True
+            transport_class, "create_channel"
         ) as grpc_create_channel:
             mock_grpc_channel = mock.Mock()
             grpc_create_channel.return_value = mock_grpc_channel
