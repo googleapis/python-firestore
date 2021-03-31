b06f1794a43b88f3899107d5dbb42018040b31eb
yoshi-automation <yoshi-automation@google.com>
2021-03-31 02:56:37 -0700

    test(python): use constraints files to check dependency lower bounds
    Use a constraints file when installing dependencies for system and unit tests nox sessions.
    
    https://pip.pypa.io/en/stable/user_guide/#constraints-files
    > Constraints files are requirements files that **only control which version of a requirement is installed, not whether it is installed or not**. Their syntax and contents is nearly identical to Requirements Files. There is one key difference: Including a package in a constraints file does not trigger installation of the package.
    
    ```
    testing
    ├── constraints-3.10.txt
    ├── constraints-3.11.txt
    ├── constraints-3.6.txt
    ├── constraints-3.7.txt
    ├── constraints-3.8.txt
    └── constraints-3.9.txt
    ```
    
    Going forward, one constraints file (currently 3.6) will be populated with every library requirement and extra listed in the `setup.py`. The constraints file will pin each requirement to the lower bound. This ensures that library maintainers will see test failures if they forget to update a lower bound on a dependency.
    
    See https://github.com/googleapis/python-bigquery/pull/263 for an example
    
    Source-Author: Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
    Source-Date: Tue Mar 23 10:52:02 2021 -0600
    Source-Repo: googleapis/synthtool
    Source-Sha: 86ed43d4f56e6404d068e62e497029018879c771
    Source-Link: https://github.com/googleapis/synthtool/commit/86ed43d4f56e6404d068e62e497029018879c771

--- noxfile.py
+++ noxfile.py
@@ -18,6 +18,7 @@
 
 from __future__ import absolute_import
 import os
+import pathlib
 import shutil
 
 import nox
@@ -30,6 +31,8 @@ BLACK_PATHS = ["docs", "google", "tests", "noxfile.py", "setup.py"]
 DEFAULT_PYTHON_VERSION = "3.8"
 SYSTEM_TEST_PYTHON_VERSIONS = ["3.7"]
 UNIT_TEST_PYTHON_VERSIONS = ["3.6", "3.7", "3.8"]
+
+CURRENT_DIRECTORY = pathlib.Path(__file__).parent.absolute()
 
 # 'docfx' is excluded since it only needs to run in 'docs-presubmit'
 nox.options.sessions = [
@@ -93,13 +96,15 @@ def lint_setup_py(session):
 
 def default(session):
     # Install all test dependencies, then install this package in-place.
-    session.install("pytest-asyncio", "aiounittest")
 
-    session.install(
-        "mock", "pytest", "pytest-cov",
+    constraints_path = str(
+        CURRENT_DIRECTORY / "testing" / f"constraints-{session.python}.txt"
     )
+    session.install("asyncmock", "pytest-asyncio", "-c", constraints_path)
 
-    session.install("-e", ".")
+    session.install("mock", "pytest", "pytest-cov", "-c", constraints_path)
+
+    session.install("-e", ".", "-c", constraints_path)
 
     # Run py.test against the unit tests.
     session.run(
@@ -126,6 +131,9 @@ def unit(session):
 @nox.session(python=SYSTEM_TEST_PYTHON_VERSIONS)
 def system(session):
     """Run the system test suite."""
+    constraints_path = str(
+        CURRENT_DIRECTORY / "testing" / f"constraints-{session.python}.txt"
+    )
     system_test_path = os.path.join("tests", "system.py")
     system_test_folder_path = os.path.join("tests", "system")
 
@@ -151,9 +159,14 @@ def system(session):
     # Install all test dependencies, then install this package into the
     # virtualenv's dist-packages.
     session.install(
-        "mock", "pytest", "pytest-asyncio", "google-cloud-testutils",
+        "mock",
+        "pytest",
+        "pytest-asyncio",
+        "google-cloud-testutils",
+        "-c",
+        constraints_path,
     )
-    session.install("-e", ".")
+    session.install("-e", ".", "-c", constraints_path)
 
     # Run py.test against the system tests.
     if system_test_exists:
