948efc9f114d9e9e8b6ec3f70cab7cb3bd2182c2
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-08-17 14:50:06 -0600

    Update owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -130,7 +130,6 @@ s.remove_staging_dirs()
 # Add templated files
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(
-python.py_samples(skip_readmes=True)
     samples=False,  # set to True only if there are samples
     system_test_python_versions=["3.7"],
     unit_test_external_dependencies=["aiounittest"],
@@ -139,6 +138,7 @@ python.py_samples(skip_readmes=True)
     cov_level=100,
     split_system_tests=True,
 )
+python.py_samples(skip_readmes=True)
 
 s.move(templated_files)
 
