4ae1f4888852f713bf034c2921d9e054d058af19
Bu Sun Kim <busunkim@google.com>
2022-03-10 23:37:43 +0000

    chore(deps): allow google-api-core v2 on v1 release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ description = "Google Cloud Firestore API client library"
 version = "1.9.0"
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 1.14.0, < 2.0.0dev",
+    "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
     "google-cloud-core >= 1.4.1, < 2.0dev",
     "pytz",
 ]
