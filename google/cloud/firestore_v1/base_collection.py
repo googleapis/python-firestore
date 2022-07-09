ad4985059ddbcb16371a634efdaddae4c11ef640
Anthonios Partheniou <partheniou@google.com>
2022-07-09 11:40:19 +0000

    remove python 3.6 specific code

--- google/cloud/firestore_v1/base_collection.py
+++ google/cloud/firestore_v1/base_collection.py
@@ -483,11 +483,6 @@ def _auto_id() -> str:
         str: A 20 character string composed of digits, uppercase and
         lowercase and letters.
     """
-    if sys.version_info < (3, 7):
-        # TODO: remove when 3.6 support is discontinued.
-        # On python 3.6, random will provide the same results when forked. Reseed
-        # on each iteration to avoid collisions.
-        random.seed()
 
     return "".join(random.choice(_AUTO_ID_CHARS) for _ in range(20))
 
