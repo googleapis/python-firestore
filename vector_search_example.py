e21ebabec126f7e8355742edf30398d356862606
Sichen Liu <sichenliu@google.com>
2024-03-25 01:38:12 +0000

    Update

--- vector_search_example.py
+++ vector_search_example.py
@@ -14,6 +14,8 @@ db = firestore.Client(
     client_options={"api_endpoint": "test-firestore.sandbox.googleapis.com"}
 )
 
+collection_id = "ccccccccc"
+
 """
 Crete the single-field index to run the vector search without a pre-filter.
 $ gcloud alpha firestore indexes composite create 
@@ -22,20 +24,30 @@ $ gcloud alpha firestore indexes composite create
   --field-config field-path=singlevectorf,vector-config='{"dimension":"1", "flat": "{}"}'   
   --project=sichenliu-nt-fs-audit
 """
-
+# d = 0
+d = 600200
 for i in range(0, 5):
     data = {
         'name': 'John Doe',
         'age': 30,
         'fs': i,
         'is_active': True,
-        'singlevectorf': Vector([i + 1.0])
+        'singlevectorf': Vector([
+            i + i + 7.0 + d, 
+            i+ i + 1.0 + d, 
+            i + i + 2.0 + d, 
+            i+ i + 3.0 + d, 
+            i+ i + 4.0 + d, 
+            i+ i + 9.0 + d,
+            i + 1.535325235235231235 + d, 
+            i + 2.321312321321 + d])
     }
-    db.collection(u'cccc').add(data)
+    # db.collection(collection_id).document("doc_{}".format(str(i))).create(data)
+    db.collection(collection_id).document("doc_{}".format(str(i))).update(data)
 
-results = db.collection(u'cccc').find_nearest(
+results = db.collection(collection_id).find_nearest(
         vector_field="singlevectorf", 
-        query_vector=Vector([1.0]),
+        query_vector=Vector([1.0, 2.0, 3.0, 4.9, 6.7, 7.8, 7.9, 4.0]),
         distance_measure=DistanceMeasure.EUCLIDEAN,
         limit=5)
 
@@ -56,7 +68,7 @@ $ gcloud alpha firestore indexes composite create
 """
 
 results = db.collection(u'cccc').where('fs', '==', 1).find_nearest(
-        vector_field="singlevectorf", 
+        vector_field="singlevectorff", 
         query_vector=Vector([1.0]),
         distance_measure=DistanceMeasure.EUCLIDEAN,
         limit=5)
