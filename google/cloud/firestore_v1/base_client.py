2cb1e71110482fcca918c807907c354b63f2af1c
Baha Aiman <bahaaiman@google.com>
2024-03-26 12:57:01 -0700

    fix: request encrypted headers

--- google/cloud/firestore_v1/base_client.py
+++ google/cloud/firestore_v1/base_client.py
@@ -265,6 +265,7 @@ class BaseClient(ClientWithProject):
                 # The emulator requires additional metadata to be set.
                 self._rpc_metadata_internal.append(("authorization", "Bearer owner"))
 
+        self._rpc_metadata_internal.append(("X-Return-Encrypted-Headers", "gfe_response_only"))
         return self._rpc_metadata_internal
 
     def collection(self, *collection_path) -> BaseCollectionReference[BaseQuery]:
