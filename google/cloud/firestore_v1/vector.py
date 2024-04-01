3d61c3c42f14fe1da2c323505041d856947521e7
Sichen Liu <sichenliu@google.com>
2024-04-01 23:53:11 +0000

    remove abc type

--- google/cloud/firestore_v1/vector.py
+++ google/cloud/firestore_v1/vector.py
@@ -13,8 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections.abc import Sequence
-from typing import List
+from typing import List, Sequence
 
 
 class Vector:
