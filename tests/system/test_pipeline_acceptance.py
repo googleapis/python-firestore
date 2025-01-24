863bd1ddd7e281d035069e32e7dd7cb2ba4b7c8f
Daniel Sanche <d.sanche14@gmail.com>
2025-01-24 15:59:49 -0800

    reconstruct pipeline expr objects

--- tests/system/test_pipeline_acceptance.py
+++ tests/system/test_pipeline_acceptance.py
@@ -21,6 +21,18 @@ def loader():
         for test in test_cases["tests"]:
             yield test
 
+def _apply_yaml_args(cls, yaml_args):
+    if isinstance(yaml_args, dict):
+        # yaml has a mapping of arguments. Treat as kwargs
+        return cls(**parse_expressions(yaml_args))
+    elif isinstance(yaml_args, list):
+        # yaml has an array of arguments. Treat as args
+        return cls(*parse_expressions(yaml_args))
+    else:
+        # yaml has a single argument
+        return cls(parse_expressions(yaml_args))
+
+
 def parse_pipeline(pipeline: list[dict[str, Any], str]):
     """
     parse a yaml list of pipeline stages into firestore.pipeline_stages.Stage classes
@@ -34,15 +46,7 @@ def parse_pipeline(pipeline: list[dict[str, Any], str]):
         # find arguments if given
         if isinstance(stage, dict):
             stage_yaml_args = stage[stage_name]
-            if isinstance(stage_yaml_args, dict):
-                # yaml has a mapping of arguments. Treat as kwargs
-                stage_obj = stage_cls(**parse_expressions(stage_yaml_args))
-            elif isinstance(stage_yaml_args, list):
-                # yaml has an array of arguments. Treat as args
-                stage_obj = stage_cls(*parse_expressions(stage_yaml_args))
-            else:
-                # yaml has a single argument
-                stage_obj = stage_cls(parse_expressions(stage_yaml_args))
+            stage_obj = _apply_yaml_args(stage_cls, stage_yaml_args)
         else:
             # yaml has no arguments
             stage_obj = stage_cls()
@@ -54,7 +58,15 @@ def parse_expressions(yaml_element: Any):
     if isinstance(yaml_element, list):
         return [parse_expressions(v) for v in yaml_element]
     elif isinstance(yaml_element, dict):
-        return {parse_expressions(k): parse_expressions(v) for k,v in yaml_element.items()}
+        if len(yaml_element) == 1 and isinstance(list(yaml_element)[0], str) and hasattr(pipeline_expressions, list(yaml_element)[0]):
+            # build pipeline expressions if possible
+            cls_str = list(yaml_element)[0]
+            cls = parse_expressions(cls_str)
+            yaml_args = yaml_element[cls_str]
+            return _apply_yaml_args(cls, yaml_args)
+        else:
+            # otherwise, return dict
+            return {parse_expressions(k): parse_expressions(v) for k,v in yaml_element.items()}
     elif isinstance(yaml_element, str) and hasattr(pipeline_expressions, yaml_element):
         return getattr(pipeline_expressions, yaml_element)
     else:
